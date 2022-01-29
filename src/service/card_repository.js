24dbc9389d1dd846a03f8afeae99c094ebc3132e
Jeewon <jeewon.yoo@g1academy.com>
2022-01-29 04:37:14 -0800

    use firebase realtime database - sync cards

--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -2,6 +2,17 @@ import firebaseApp from './firebase';
 
 class CardRepository {
     // 우선 API 생각하자
+    syncCards(userId, onUpdate) {
+        const ref = firebaseApp.database().ref(`${userId}/cards`);
+        ref.on('value', snapshot => {
+            const value=snapshot.val();
+            value && onUpdate(value);
+        });
+
+        return () => {
+            ref.off();
+        }
+    }
     saveCard(userId, card) {
         firebaseApp.database().ref(`${userId}/cards/${card.id}`).set(card);
     }
